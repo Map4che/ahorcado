0631a9a07f465c79d2781048a3dce9561897d5f0
Map4che <ljuan648l@gmail.com>
2023-07-09 22:18:40 -0500

    arreglos en dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-
+  base: isProduction ? '/ahorcado/' : '/',
+  assetsDir: 'assets',
 })
