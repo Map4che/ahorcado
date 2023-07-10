df305abbe063d1187ddb1c4a06fa3402bc421c0f
Map4che <ljuan648l@gmail.com>
2023-07-10 11:27:20 -0500

    Probando nuevo base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   plugins: [react()],
-  base: isProduction ? '/ahorcado/' : '/',
-  assetsDir: 'assets',
+  base: './'
 })
